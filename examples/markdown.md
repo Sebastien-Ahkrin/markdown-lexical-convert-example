35dc8696341bdb9d4c642807b662040d93c43322
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-01-30 12:16:33 +0100

    feat: add default script to test

---
+++ examples/markdown.md
@@ -0,0 +1,29 @@
+# Heading level 1
+
+## Heading level 2
+
+I really like using Markdown.
+
+I think I'll use it to format all of my documents from now on.
+
+I just love **bold text**.
+Italicized text is the _cat's meow_.
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+1. First item
+2. Second item
+3. Third item
+4. Fourth item
+
+- First item
+- Second item
+- Third item
+- Fourth item
+
+* This is the first list item.
+* Here's the second list item.
+
+  > A blockquote would look great below the second list item.
+
+* And here's the third list item.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log("Hello, World!");
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+import { CodeHighlightNode, CodeNode } from '@lexical/code';
+import { HashtagNode } from '@lexical/hashtag';
+import { createHeadlessEditor } from '@lexical/headless';
+import { LinkNode, AutoLinkNode } from '@lexical/link';
+import { ListNode, ListItemNode } from '@lexical/list';
+import { MarkNode } from '@lexical/mark';
+import { $convertFromMarkdownString, TRANSFORMERS } from '@lexical/markdown';
+import { OverflowNode } from '@lexical/overflow';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { TableNode, TableCellNode, TableRowNode } from '@lexical/table';
+import { LineBreakNode, ParagraphNode, TextNode } from 'lexical';
+
+async function getMarkdownFile() {
+  const filePath = join(__dirname, '../examples/markdown.md');
+  const data = await readFile(filePath, 'utf-8');
+  return data;
+}
+
+async function run() {
+  const markdown = await getMarkdownFile();
+
+  const editor = createHeadlessEditor({
+    nodes: [
+      LineBreakNode,
+      ParagraphNode,
+      TextNode,
+      LinkNode,
+      HeadingNode,
+      QuoteNode,
+      ListNode,
+      ListItemNode,
+      CodeHighlightNode,
+      CodeNode,
+      HashtagNode,
+      MarkNode,
+      OverflowNode,
+      TableNode,
+      TableCellNode,
+      TableRowNode,
+      AutoLinkNode,
+    ],
+  });
+
+  // eslint-disable-next-line @typescript-eslint/await-thenable
+  await editor.update(() => {
+    $convertFromMarkdownString(markdown, TRANSFORMERS);
+  });
+
+  return editor.getEditorState().toJSON();
+}
+
+// eslint-disable-next-line no-console
+run().then(console.log).catch(console.error);
